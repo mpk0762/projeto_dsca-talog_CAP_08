91b6fc293723baa5957d3c5d8e3ad414641fee6e
mpk0762 <mpk86647@gmail.com>
2022-06-10 19:27:43 -0300

    pagination, styling

--- frontweb/src/components/Pagination/index.tsx
+++ frontweb/src/components/Pagination/index.tsx
@@ -1,14 +1,16 @@
 import { ReactComponent as ArrowIcon } from 'assets/images/arrow.svg';
+import './styles.css';
 
 const Pagination = () => {
   return (
-    <div>
-      <ArrowIcon />
-      <div className="pagination-item">1</div>
+    <div className="pagination-container">
+      <ArrowIcon className="arrow-inactive arrow-previos" />
+      <div className="pagination-item active">1</div>
       <div className="pagination-item">2</div>
       <div className="pagination-item">3</div>
       <div className="pagination-item">...</div>
       <div className="pagination-item">10</div>
+      <ArrowIcon className="arrow-active arrow-next " />
     </div>
   );
 };
